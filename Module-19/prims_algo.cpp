ce6aef1123e842d7b1a80528d708ea20d586b21e
rafiqul0004 <kr.rafiqul@gmail.com>
2023-08-30 20:01:11 +0600

    Module-19

---
+++ Module-19/prims_algo.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> pii;
+const int N = 1e5 + 5;
+vector<pii> v[N];
+bool visited[N];
+class Edge
+{
+public:
+    int a, b, w;
+    Edge(int a, int b, int w)
+    {
+        this->a = a;
+        this->b = b;
+        this->w = w;
+    }
+};
+class cmp
+{
+public:
+    bool operator()(Edge a, Edge b)
+    {
+        return a.w > b.w;
+    }
+};
+void prims(int s)
+{
+    priority_queue<Edge, vector<Edge>, cmp> pq;
+    vector<Edge> edge_list;
+    pq.push(Edge(s, s, 0));
+    while (!pq.empty())
+    {
+        Edge parent = pq.top();
+        pq.pop();
+        int a = parent.a;
+        int b = parent.b;
+        int w = parent.w;
+        if (visited[b])
+            continue;
+        visited[b] = true;
+        edge_list.push_back(parent);
+        for (int i = 0; i < v[b].size(); i++)
+        {
+            pii child = v[b][i];
+            if (!visited[child.first])
+            {
+                pq.push(Edge(b, child.first, child.second));
+            }
+        }
+    }
+    edge_list.erase(edge_list.begin());
+    for (Edge val : edge_list)
+    {
+        cout << val.a << " " << val.b << " " << val.w << endl;
+    }
+}
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b, w;
+        cin >> a >> b >> w;
+        v[a].push_back({b, w});
+        v[b].push_back({a, w});
+    }
+    prims(1);
+    return 0;
+}
