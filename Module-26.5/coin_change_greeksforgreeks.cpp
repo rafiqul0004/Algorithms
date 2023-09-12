3429b11ebe86806a880bb8d9d2782cb3f88b644c
rafiqul0004 <kr.rafiqul@gmail.com>
2023-09-12 11:51:45 +0600

    Module

---
+++ Module-26.5/coin_change_greeksforgreeks.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    long long int count(int coins[], int N, int sum)
+    {
+
+        long long int dp[N + 1][sum + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= sum; i++)
+        {
+            dp[0][i] = 0;
+        }
+        for (int i = 1; i <= N; i++)
+        {
+            for (int j = 0; j <= sum; j++)
+            {
+                if (coins[i - 1] <= j)
+                {
+                    dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
+                }
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[N][sum];
+    }
+};
---
+++ Module-26.5/leetcode_coin_change.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    int coinChange(vector<int> &coins, int amount)
+    {
+        int n = coins.size();
+        int dp[n + 1][amount + 1];
+        dp[0][0] = 0;
+        for (int i = 1; i <= amount; i++)
+        {
+            dp[0][i] = INT_MAX - 1;
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 0; j <= amount; j++)
+            {
+                if (coins[i - 1] <= j)
+                {
+                    dp[i][j] = min(dp[i][j - coins[i - 1]] + 1, dp[i - 1][j]);
+                }
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+
+        if (dp[n][amount] != INT_MAX - 1)
+            return dp[n][amount];
+        else
+            return -1;
+    }
+};
